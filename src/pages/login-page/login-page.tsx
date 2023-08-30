d0be89aeeee86bbb700097d8c28aead94f37b52e
Isidro Merayo <isidro.merayo@jcyl.es>
2023-08-30 14:42:40 +0200

    refactor Yup

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,5 +1,12 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Typography, TextField, Button } from '@mui/material'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const loginSchema = yup.object({
+    email: yup.string().required('The email is required'),
+    password: yup.string().required('The password is required'),
+  }).required();
 
 interface Inputs {
     email: string,
@@ -7,7 +14,13 @@ interface Inputs {
 };
 
 export const LoginPage = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const { 
+        register, 
+        handleSubmit, 
+        formState: { errors } 
+    } = useForm<Inputs>({
+        resolver: yupResolver(loginSchema)
+    });
 
     const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
@@ -15,8 +28,8 @@ export const LoginPage = () => {
         <>
         <Typography component="h1">Login</Typography>
         <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField label="Email" {...register(('email'), {required: true })} helperText={errors.email && 'The email is required'}></TextField>
-        <TextField label="Password" type="password" {...register(('password'), {required: true })} helperText={errors.password && 'The password is required'}/>
+        <TextField label="Email" {...register(('email'), {required: true })} helperText={errors.email?.message}></TextField>
+        <TextField label="Password" type="password" {...register(('password'), {required: true })} helperText={errors.password?.message} />
         <Button type="submit">Submit</Button>
         </form>
     </>

11e7b61ec89f9526f5cd56f5224f554064055e83
Isidro Merayo <isidro.merayo@jcyl.es>
2023-08-30 14:47:52 +0200

    jugando con el duplicado en los test

--- src/__test__/pages/login-page/login-page.spec.tsx
+++ src/__test__/pages/login-page/login-page.spec.tsx
@@ -2,6 +2,8 @@ import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { LoginPage } from '../../../pages/login-page/login-page';
 
+const getSubmitBtn = () => screen.getByRole('button', {name: /submit/i})
+
 describe('Login page', () => {
    
     it('should be render login title', () => {
@@ -9,19 +11,20 @@ describe('Login page', () => {
         
         expect(screen.getByRole('heading', {name: /login/i} )).toBeInTheDocument()
     });
+    
     it('should render the form elements', () => {
         render(<LoginPage/>)
         
         expect(screen.getByLabelText(/email/i) ).toBeInTheDocument()
         expect(screen.getByLabelText(/email/i) ).toBeInTheDocument()
-        expect(screen.getByRole('button', {name: /submit/i}) ).toBeInTheDocument()
+        expect(getSubmitBtn()).toBeInTheDocument()
     });
 
     it('should validate the inputs as required', async () => {
         render(<LoginPage/>)
         
         // submit form
-        userEvent.click(screen.getByRole('button', {name: /submit/i}))
+        userEvent.click(getSubmitBtn())
 
         // expect validation errros
         expect(await screen.findByText(/The email is required/i)).toBeInTheDocument()

10e38622af463da1b195c6a80012ecbcf8673e05
Isidro Merayo <isidro.merayo@jcyl.es>
2023-08-30 11:19:39 +0200

    red

--- src/__test__/pages/login-page/login-page.spec.tsx
+++ src/__test__/pages/login-page/login-page.spec.tsx
@@ -4,10 +4,14 @@ import { LoginPage } from '../../../pages/login-page/login-page';
 
 describe('Login page', () => {
    
-    it('should behave...', () => {
+    it('should be render login title', () => {
         render(<LoginPage/>)
         
         expect(screen.getByRole('heading', {name: /login/i} )).toBeInTheDocument()
-
+    });
+    it('should render the form elements', () => {
+        render(<LoginPage/>)
+        
+        expect(screen.getByLabelText(/email/i) ).toBeInTheDocument()
     });
 });

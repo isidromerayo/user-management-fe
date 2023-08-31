9b0c94c498c0b96b5dcda920aaf0872fe0e666ed
Isidro Merayo <isidro.merayo@jcyl.es>
2023-08-31 13:41:16 +0200

    axios y revisión

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,5 +3,5 @@ import {rest} from 'msw'
 export const handlers = [
   rest.post('/login', (req, res, ctx) => res(ctx.status(200))),
   // Handles a GET /user request
-  rest.get('/user', null),
+  // rest.get('/user', null),
 ]
